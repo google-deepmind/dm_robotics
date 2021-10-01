5f10a8aa2dedee4c14c8922332c2a008072821ee
David Khosid <dkhosid@google.com>
2021-10-01 11:10:29 +0100

    Remove inheritance from Callable.
    PiperOrigin-RevId: 400156100
    Change-Id: If9ff0868af34e1fc6d96e60742458eb497d5eb80

--- py/manipulation/props/parametric_object/parametric_object.py
+++ py/manipulation/props/parametric_object/parametric_object.py
@@ -25,7 +25,7 @@ import collections
 import enum
 import logging
 import re
-from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple, Union
 
 
 @enum.unique
@@ -149,160 +149,7 @@ class ParametersDict(collections.UserDict):
     return self._param_types
 
 
-class ParametricProperties:
-  """A class to parametrically describe the parametric properties of an object.
-
-  ParametricProperties class manipulates parametric properties. A parametric
-  property is the collection of two things: (1) the parameters names in the form
-  of a N-tuple of string (e.g. ('param1_name', ..., 'paramN_name')); (2) a
-  function which takes a dict (e.g. {'param1_name': val1, ...,
-  'paramN_name': valN}) and checks if the given values are valid. Both the
-  param names and the function are specified in the constructor. A simple
-  example of a parametric object can be a cube which has only one parameter
-  (the length of the cube edge); the validity check is for this parameter to be
-  grather than zero.
-  """
-
-  def __init__(
-      self,
-      param_names: Tuple[str],
-      param_bounds_function: Callable[[ParametersDict], bool],
-      param_units: Optional[Tuple[ParametersUnits]] = None,
-      param_types: Optional[Tuple[ParametersTypes]] = None) -> None:
-    """The ParametricShape class initialiazer.
-
-    Args:
-      param_names: a tuple containing the parameter names
-      param_bounds_function: a function that returns a bool given a dictionary
-        which defines an instance of a parametric object. This function returns
-        true if the given instance is valid (i.e. inside the bounds), false
-        otherwise. The second element is the max value for the given parameter.
-      param_units: a tuple containing the unit of measure for the provided
-        parameters. If the parameter is a length accepted units are
-        millimeter, centimeter and meter (i.e. 'mm', 'cm', 'm'). If the
-        parameter is an angle accepted units are degree and radian (i.e. 'deg',
-        'rad'). If the parameter is adimensional then an empty string should be
-        provided. If no param_units is provided all parameters are
-        considered adimensional.
-      param_types: a tuple containing the types (int/float) for the provided
-        parameters. If no param_types is provided all parameters are
-        considered integers.
-    """
-    self._param_bounds_function = param_bounds_function
-    self._param_names = param_names
-    if param_units is None:
-      self._param_units = (ParametersUnits.ADIMENSIONAL,) * len(
-          self._param_names)
-    else:
-      self._param_units = param_units
-
-    if param_types is None:
-      self._param_types = (ParametersTypes.INTEGER,) * len(self._param_names)
-    else:
-      self._param_types = param_types
-
-  def check_instance(self, values: ParametersDict) -> bool:
-    """A function to check the validity of the provided parameters.
-
-    The function requires a dictionary which contain all parameter values.
-    The dictionary is structured as follows:
-    {'par1': values[0], ..., 'parN': values[N-1]}
-
-    Args:
-      values: a dictionary ['params': value] for property.
-
-    Returns:
-      true/false depending on the instanaced values being valid or not.
-    """
-    return self._param_bounds_function(values)
-
-  def get_name(self, property_dict: ParametersDict) -> str:
-    """Creates a string which can be used as name for a parametric property.
-
-    The input dictionary should have the following structure:
-                {'par1': values[0], ..., 'parN': values[N-1]}
-    Args:
-      property_dict: a dictionary containing ['params': value].
-
-    Returns:
-      string with the names of the parameters separated by '_'.
-    """
-    self.check_instance(property_dict)
-    return '_'.join('{}{}'.format(k, v) for k, v in property_dict.items())
-
-  @property
-  def param_names(self) -> Tuple[str, ...]:
-    """Returns the tuple that contains the names of the parameters.
-
-    Returns:
-      tuple with the names of the parameters.
-    """
-    return self._param_names
-
-  @property
-  def param_types(self) -> Tuple[ParametersTypes]:
-    """Returns the tuple that contains the types of the parameters.
-
-    Returns:
-      tuple with the types of the parameters.
-    """
-    return self._param_types
-
-  @property
-  def param_units(self) -> Tuple[ParametersUnits]:
-    """Returns the tuple that contains the units of the parameters.
-
-    Returns:
-      tuple with the units of the parameters.
-    """
-    return self._param_units
-
-  def get_dict(self, param_string: str) -> ParametersDict:
-    """Creates a dict given the string that describes the parametric properties.
-
-    The input string should have the following structure:
-                'par1values[0]_ ... _parNvalues[N-1]'
-    The output dict will have the following structure:
-                {'par1': values[0], ..., 'parN': values[N-1]}
-    Numeric values (i.e. values[0], ..., values[N-1]) can be `int` or `float`.
-    If `float` we assume the number is represented with a string of the form x.y
-    (i.e. `first0.01_second3_third12.25` is converted to {'first': 0.01,
-    'second': 3, 'third': 12.25}).
-    Args:
-      param_string: a srting that describes the parametric property.
-
-    Returns:
-      a dict that describes the parametric property.
-    """
-    array_names = re.findall('[a-zA-Z]+', param_string)
-    array_values = re.findall(r'(\d+(?:\.\d+)?)', param_string)
-    array = param_string.split('_')
-
-    if len(array_names) != len(array_values):
-      raise ValueError(f'Something wrong converting: {param_string}.')
-    if len(array_names) != len(array):
-      raise ValueError(f'Something wrong with delimiters: {param_string}.')
-
-    shape_dict = ParametersDict({})
-    iter_names = iter(self.param_names)
-    for value, name in zip(array_values, array_names):
-      if name != next(iter_names):
-        raise ValueError(f'Wrong parameters in given string: {param_string}.')
-      try:
-        shape_dict[name] = int(value)
-      except ValueError:
-        try:
-          shape_dict[name] = float(value)
-        except ValueError:
-          raise ValueError(f'Cannot convert {value} to int or float.')
-    try:
-      iter_names.__next__()
-    except StopIteration:
-      return shape_dict
-    raise ValueError('Error: not all parameters were provided.')
-
-
-class ParametricMinMaxBounds(Callable):
+class ParametricMinMaxBounds():
   """A class to parametrically describe the parametric properties of an object.
 
   Each parameter is specified with three quantities which correspond
@@ -455,6 +302,159 @@ class ParametricMinMaxBounds(Callable):
           return False
 
     return True
+
+
+class ParametricProperties:
+  """A class to parametrically describe the parametric properties of an object.
+
+  ParametricProperties class manipulates parametric properties. A parametric
+  property is the collection of two things: (1) the parameters names in the form
+  of a N-tuple of string (e.g. ('param1_name', ..., 'paramN_name')); (2) a
+  function which takes a dict (e.g. {'param1_name': val1, ...,
+  'paramN_name': valN}) and checks if the given values are valid. Both the
+  param names and the function are specified in the constructor. A simple
+  example of a parametric object can be a cube which has only one parameter
+  (the length of the cube edge); the validity check is for this parameter to be
+  grather than zero.
+  """
+
+  def __init__(
+      self,
+      param_names: Tuple[str],
+      param_bounds_function: ParametricMinMaxBounds,
+      param_units: Optional[Tuple[ParametersUnits]] = None,
+      param_types: Optional[Tuple[ParametersTypes]] = None) -> None:
+    """The ParametricShape class initialiazer.
+
+    Args:
+      param_names: a tuple containing the parameter names
+      param_bounds_function: a function that returns a bool given a dictionary
+        which defines an instance of a parametric object. This function returns
+        true if the given instance is valid (i.e. inside the bounds), false
+        otherwise. The second element is the max value for the given parameter.
+      param_units: a tuple containing the unit of measure for the provided
+        parameters. If the parameter is a length accepted units are
+        millimeter, centimeter and meter (i.e. 'mm', 'cm', 'm'). If the
+        parameter is an angle accepted units are degree and radian (i.e. 'deg',
+        'rad'). If the parameter is adimensional then an empty string should be
+        provided. If no param_units is provided all parameters are
+        considered adimensional.
+      param_types: a tuple containing the types (int/float) for the provided
+        parameters. If no param_types is provided all parameters are
+        considered integers.
+    """
+    self._param_bounds_function = param_bounds_function
+    self._param_names = param_names
+    if param_units is None:
+      self._param_units = (ParametersUnits.ADIMENSIONAL,) * len(
+          self._param_names)
+    else:
+      self._param_units = param_units
+
+    if param_types is None:
+      self._param_types = (ParametersTypes.INTEGER,) * len(self._param_names)
+    else:
+      self._param_types = param_types
+
+  def check_instance(self, values: ParametersDict) -> bool:
+    """A function to check the validity of the provided parameters.
+
+    The function requires a dictionary which contain all parameter values.
+    The dictionary is structured as follows:
+    {'par1': values[0], ..., 'parN': values[N-1]}
+
+    Args:
+      values: a dictionary ['params': value] for property.
+
+    Returns:
+      true/false depending on the instanaced values being valid or not.
+    """
+    return self._param_bounds_function(values)
+
+  def get_name(self, property_dict: ParametersDict) -> str:
+    """Creates a string which can be used as name for a parametric property.
+
+    The input dictionary should have the following structure:
+                {'par1': values[0], ..., 'parN': values[N-1]}
+    Args:
+      property_dict: a dictionary containing ['params': value].
+
+    Returns:
+      string with the names of the parameters separated by '_'.
+    """
+    self.check_instance(property_dict)
+    return '_'.join('{}{}'.format(k, v) for k, v in property_dict.items())
+
+  @property
+  def param_names(self) -> Tuple[str, ...]:
+    """Returns the tuple that contains the names of the parameters.
+
+    Returns:
+      tuple with the names of the parameters.
+    """
+    return self._param_names
+
+  @property
+  def param_types(self) -> Tuple[ParametersTypes]:
+    """Returns the tuple that contains the types of the parameters.
+
+    Returns:
+      tuple with the types of the parameters.
+    """
+    return self._param_types
+
+  @property
+  def param_units(self) -> Tuple[ParametersUnits]:
+    """Returns the tuple that contains the units of the parameters.
+
+    Returns:
+      tuple with the units of the parameters.
+    """
+    return self._param_units
+
+  def get_dict(self, param_string: str) -> ParametersDict:
+    """Creates a dict given the string that describes the parametric properties.
+
+    The input string should have the following structure:
+                'par1values[0]_ ... _parNvalues[N-1]'
+    The output dict will have the following structure:
+                {'par1': values[0], ..., 'parN': values[N-1]}
+    Numeric values (i.e. values[0], ..., values[N-1]) can be `int` or `float`.
+    If `float` we assume the number is represented with a string of the form x.y
+    (i.e. `first0.01_second3_third12.25` is converted to {'first': 0.01,
+    'second': 3, 'third': 12.25}).
+    Args:
+      param_string: a srting that describes the parametric property.
+
+    Returns:
+      a dict that describes the parametric property.
+    """
+    array_names = re.findall('[a-zA-Z]+', param_string)
+    array_values = re.findall(r'(\d+(?:\.\d+)?)', param_string)
+    array = param_string.split('_')
+
+    if len(array_names) != len(array_values):
+      raise ValueError(f'Something wrong converting: {param_string}.')
+    if len(array_names) != len(array):
+      raise ValueError(f'Something wrong with delimiters: {param_string}.')
+
+    shape_dict = ParametersDict({})
+    iter_names = iter(self.param_names)
+    for value, name in zip(array_values, array_names):
+      if name != next(iter_names):
+        raise ValueError(f'Wrong parameters in given string: {param_string}.')
+      try:
+        shape_dict[name] = int(value)
+      except ValueError:
+        try:
+          shape_dict[name] = float(value)
+        except ValueError:
+          raise ValueError(f'Cannot convert {value} to int or float.')
+    try:
+      iter_names.__next__()
+    except StopIteration:
+      return shape_dict
+    raise ValueError('Error: not all parameters were provided.')
 
 
 class ParametricObject:
