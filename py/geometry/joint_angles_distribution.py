d814ee60ec980711f1fe6f07c380b73cb4be4b3e
Default <no-reply@google.com>
2022-12-09 03:27:30 -0800

    Make unused `physics` argument optional.
    PiperOrigin-RevId: 494127707
    Change-Id: Icf9b52d7bf0233d55b8c5ff7fdc1da6614321d61

--- py/geometry/joint_angles_distribution.py
+++ py/geometry/joint_angles_distribution.py
@@ -14,6 +14,8 @@
 """A module for sampling joint angles distributions."""
 
 import abc
+from typing import Optional
+
 from dm_robotics.geometry import geometry
 import numpy as np
 
@@ -23,7 +25,7 @@ class JointAnglesDistribution(abc.ABC):
 
   @abc.abstractmethod
   def sample_angles(self, random_state,
-                    physics: geometry.Physics) -> np.ndarray:
+                    physics: Optional[geometry.Physics] = None) -> np.ndarray:
     """Returns angles sampled from some distribution."""
     pass
 
@@ -36,7 +38,7 @@ class ConstantPanTiltDistribution(JointAnglesDistribution):
     self._joint_angles = joint_angles
 
   def sample_angles(self, random_state,
-                    physics: geometry.Physics) -> np.ndarray:
+                    physics: Optional[geometry.Physics] = None) -> np.ndarray:
     return np.array(self._joint_angles, dtype=np.float32)
 
 
@@ -50,7 +52,7 @@ class NormalOffsetJointAnglesDistribution(JointAnglesDistribution):
     self._clip_sd = clip_sd
 
   def sample_angles(self, random_state,
-                    physics: geometry.Physics) -> np.ndarray:
+                    physics: Optional[geometry.Physics] = None) -> np.ndarray:
     offset = random_state.normal(scale=self._angles_sd)
     clip_range = self._angles_sd * self._clip_sd
     return self._mean_angles + np.clip(offset, -clip_range, clip_range)
@@ -65,7 +67,7 @@ class UniformJointAnglesDistribution(JointAnglesDistribution):
     self._max_angles = max_angles
 
   def sample_angles(self, random_state,
-                    physics: geometry.Physics) -> np.ndarray:
+                    physics: Optional[geometry.Physics] = None) -> np.ndarray:
     joint_angles = random_state.uniform(
         low=self._min_angles, high=self._max_angles)
     return np.array(joint_angles, dtype=np.float32)
